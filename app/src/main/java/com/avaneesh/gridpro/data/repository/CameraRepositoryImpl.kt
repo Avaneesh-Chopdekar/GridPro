6154fd80332d7c7bf8e27279e20f7cf21d4e7c96
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-09-14 12:25:55 +0530

    create takePhoto() function

--- app/src/main/java/com/avaneesh/gridpro/data/repository/CameraRepositoryImpl.kt
+++ app/src/main/java/com/avaneesh/gridpro/data/repository/CameraRepositoryImpl.kt
@@ -1,16 +1,51 @@
 package com.avaneesh.gridpro.data.repository
 
 import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.video.Recording
 import androidx.camera.view.LifecycleCameraController
+import androidx.core.content.ContextCompat
 import com.avaneesh.gridpro.domain.repository.CameraRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CameraRepositoryImpl @Inject constructor(
     private val application: Application
 ): CameraRepository {
 
+    private val recording: Recording? = null
     override suspend fun takePhoto(controller: LifecycleCameraController) {
-        TODO("Not yet implemented")
+        controller.takePicture(
+            ContextCompat.getMainExecutor(application),
+            object : ImageCapture.OnImageCapturedCallback() {
+
+                override fun onCaptureSuccess(imageProxy: ImageProxy) {
+                    super.onCaptureSuccess(imageProxy)
+
+                    val matrix = Matrix().apply {
+                        postRotate(imageProxy.imageInfo.rotationDegrees.toFloat())
+                    }
+
+                    val imageBitmap = Bitmap.createBitmap(
+                        imageProxy.toBitmap(),
+                        0,0,
+                        imageProxy.width,
+                        imageProxy.height,
+                        matrix,
+                        true
+                    )
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        saveImage(imageBitmap)
+                    }
+                }
+            }
+        )
     }
 
     override suspend fun recordVideo(controller: LifecycleCameraController) {
