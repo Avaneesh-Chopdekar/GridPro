ed3ccfa0c182c2d0a5189fff2bb0cf447112227d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-09-14 17:25:19 +0530

    create saveImage() function

--- app/src/main/java/com/avaneesh/gridpro/data/repository/CameraRepositoryImpl.kt
+++ app/src/main/java/com/avaneesh/gridpro/data/repository/CameraRepositoryImpl.kt
@@ -2,8 +2,16 @@ package com.avaneesh.gridpro.data.repository
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.graphics.Bitmap
 import android.graphics.Matrix
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
@@ -14,10 +22,12 @@ import androidx.camera.video.VideoRecordEvent
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.video.AudioConfig
 import androidx.core.content.ContextCompat
+import com.avaneesh.gridpro.R
 import com.avaneesh.gridpro.domain.repository.CameraRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import javax.inject.Inject
 
@@ -48,7 +58,16 @@ class CameraRepositoryImpl @Inject constructor(
                     )
 
                     CoroutineScope(Dispatchers.IO).launch {
-                        saveImage(imageBitmap)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            saveImage(imageBitmap)
+                        } else {
+                            Toast.makeText(
+                                application,
+                                "You need minimum Android 10 to save media",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+
                     }
                 }
             }
@@ -78,6 +97,60 @@ class CameraRepositoryImpl @Inject constructor(
                     CoroutineScope(Dispatchers.IO).launch {
                         saveVideo(file)
                     }
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private suspend fun saveImage(bitmap: Bitmap) {
+        withContext(Dispatchers.IO) {
+            val resolver: ContentResolver = application.contentResolver
+
+            val imageCollection = MediaStore.Images.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL_PRIMARY
+            )
+
+            val appName = application.getString(R.string.app_name)
+            val timeInMillis = System.currentTimeMillis()
+
+            val imageContentValues: ContentValues = ContentValues().apply {
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    "${timeInMillis}_image" + ".jpg"
+                )
+                put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    Environment.DIRECTORY_DCIM + "/$appName"
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+                put(MediaStore.MediaColumns.DATE_TAKEN, timeInMillis)
+                put(MediaStore.MediaColumns.IS_PENDING, 1)
+            }
+
+            val imageMediaStoreUri: Uri? = resolver.insert(
+                imageCollection, imageContentValues
+            )
+
+            imageMediaStoreUri?.let { uri ->
+                try {
+                    resolver.openOutputStream(uri)?.let { outputStream ->
+                        bitmap.compress(
+                            Bitmap.CompressFormat.JPEG, 100, outputStream
+                        )
+                    }
+
+                    imageContentValues.clear()
+                    imageContentValues.put(
+                        MediaStore.MediaColumns.IS_PENDING, 0
+                    )
+                    resolver.update(
+                        uri, imageContentValues, null, null
+                    )
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    resolver.delete(uri, null, null)
                 }
             }
         }
